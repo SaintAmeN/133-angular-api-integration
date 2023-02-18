74d17185f9622480b9c5331ddc2321ebe04cd60b
Paweł Recław <pawel.reclaw@gmail.com>
2023-02-18 11:35:34 +0100

    Dodana strona generowania

--- src/app/page-facts-generator/page-facts-generator.component.ts
+++ src/app/page-facts-generator/page-facts-generator.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { CatFact, CATS_API_BASE_URL, CATS_API_ENDPOINT_FACTS_RANDOM } from '../cats-api/model';
 
 @Component({
   selector: 'app-page-facts-generator',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-facts-generator.component.css']
 })
 export class PageFactsGeneratorComponent {
+  generatedFact: CatFact | null = null
 
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  generateFact(type: string) {
+    this.httpClient.get<CatFact>(CATS_API_BASE_URL + CATS_API_ENDPOINT_FACTS_RANDOM,
+      {
+        params: {
+          'animal_type': type
+        }
+      })
+      .subscribe({
+        next: data => {
+          this.generatedFact = data
+        },
+        error: err => {
+          console.log('Error loading cat fact: ' + err)
+        }
+      })
+  }
 }
